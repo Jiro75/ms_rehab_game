d780fa0ad6c4c44e1458a9f2d854685668661593
gannaahmed200 <24680246cba@gmail.com>
2026-04-18 12:13:28 +0200

    FIXED

--- launch_game.py
+++ launch_game.py
@@ -1,5 +1,9 @@
+import matplotlib
+matplotlib.use("Agg")  # prevent tkinter/threading crash on Windows
+ 
 from ms_rehab_game.main import main
-
-
+ 
+ 
 if __name__ == "__main__":
     raise SystemExit(main())
+ 
